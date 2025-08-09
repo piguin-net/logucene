afdee13aa747fb4ad586f84fbd133e71990b4065
piguin <piguin.net@gmail.com>
2025-08-09 20:29:41 +0900

    webサーバの処理をメソッドへ切り出し

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,7 +2,6 @@ package com.example;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -28,6 +27,7 @@ import org.slf4j.LoggerFactory;
 import com.example.LuceneManager.LuceneFieldKeys;
 
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
 import io.javalin.http.staticfiles.Location;
 
@@ -68,103 +68,115 @@ public class Main
         }
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException {
+    public static void main(String[] args) {
         Javalin server = Javalin.create(config -> {
             config.staticFiles.add(staticFiles -> {
                 staticFiles.hostedPath = "/";
                 staticFiles.directory = "/public";
                 staticFiles.location = Location.CLASSPATH;
             });
-        }).get("/api/search", ctx -> {
-            SearchResult result = new SearchResult();
-            try {
-                String query = ctx.queryParam("query");
-                TopDocs hits = lucene.search(
-                    LuceneFieldKeys.message.name(),
-                    query == null || "".equals(query.trim()) ? "*:*" : query,
-                    new Sort(new SortedNumericSortField(
-                        LuceneFieldKeys.timestamp.name(),
-                        SortField.Type.LONG,
-                        true
-                    ))
-                );
-                result.total = hits.totalHits.value();
-                result.ids = Arrays.asList(
-                    hits.scoreDocs
-                ).stream().map(
-                    hit -> hit.doc
-                ).collect(
-                    Collectors.toList()
-                );
-                cache.put(result.uuid, result.ids);
-                ctx.json(result);
-            } catch (IndexNotFoundException e) {
-                logger.atWarn().log("index not found.");
-                cache.put(result.uuid, new ArrayList<>());
-            } catch (Exception e) {
-                ctx.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e);
-            }
-        }).get("/api/documents", ctx -> {
-            try {
-                UUID uuid = UUID.fromString(ctx.queryParam("uuid"));
-                Integer first = Integer.valueOf(ctx.queryParam("first"));
-                Integer last = Integer.valueOf(ctx.queryParam("last"));
-                List<Integer> ids = cache.get(uuid).subList(
-                    first,
-                    last > cache.get(uuid).size()
-                        ? cache.get(uuid).size()
-                        : last
-                );
-                List<Map.Entry<Integer,Document>> docs = lucene.documents(ids);
-                List<Map<String, Object>> result = docs.stream().map(doc -> {
-                    Map<String, Object> dict = new HashMap<>() {{
-                        this.put("id", doc.getKey());
-                        for (IndexableField field: doc.getValue().getFields()) {
-                            this.put(field.name(), doc.getValue().get(field.name()));
-                        }
-                    }};
-                    return dict;
-                }).collect(Collectors.toList());
-                ctx.json(result);
-            } catch (IndexNotFoundException e) {
-                logger.atWarn().log("index not found.");
-            } catch (Exception e) {
-                ctx.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e);
-            }
-        }).get("/api/download", ctx -> {
-            UUID uuid = UUID.fromString(ctx.queryParam("uuid"));
-            List<Map.Entry<Integer,Document>> docs = lucene.documents(cache.get(uuid));
-
-            try (Workbook book = new XSSFWorkbook();) {
-                Sheet sheet = book.createSheet();
-                int x = 0;
-                int y = 0;
-                Row header = sheet.createRow(y++);
-                header.createCell(x++).setCellValue("id");
-                for (LuceneFieldKeys field: LuceneFieldKeys.values()) {
-                    header.createCell(x++).setCellValue(field.name());
-                }
-                for (Map.Entry<Integer,Document> doc: docs) {
-                    x = 0;
-                    Row row = sheet.createRow(y++);
-                    row.createCell(x++).setCellValue(doc.getKey());
-                    for (LuceneFieldKeys field: LuceneFieldKeys.values()) {
-                        row.createCell(x++).setCellValue(doc.getValue().get(field.name()));
-                    }
-                }
-                ByteArrayOutputStream buf = new ByteArrayOutputStream();
-                book.write(buf);
-                ctx.contentType(
-                    "application/octet-stream"
-                ).header(
-                    "Content-Disposition", "attachment; filename=\"syslog.xlsx\""
-                ).result(
-                    buf.toByteArray()
-                );
-            }
-        }).before(ctx -> {
-            logger.atInfo().log(ctx.fullUrl());
-        });
+        }).get(
+            "/api/search", Main::search
+        ).get(
+            "/api/documents", Main::documents
+        ).get(
+            "/api/download", Main::download
+        ).before(
+            ctx -> logger.atInfo().log(ctx.fullUrl())
+        );
         server.start(Integer.getInteger("web.port", 8080));
+    }
+
+    private static void search(Context ctx) {
+        SearchResult result = new SearchResult();
+        try {
+            String query = ctx.queryParam("query");
+            TopDocs hits = lucene.search(
+                LuceneFieldKeys.message.name(),
+                query == null || "".equals(query.trim()) ? "*:*" : query,
+                new Sort(new SortedNumericSortField(
+                    LuceneFieldKeys.timestamp.name(),
+                    SortField.Type.LONG,
+                    true
+                ))
+            );
+            result.total = hits.totalHits.value();
+            result.ids = Arrays.asList(
+                hits.scoreDocs
+            ).stream().map(
+                hit -> hit.doc
+            ).collect(
+                Collectors.toList()
+            );
+            cache.put(result.uuid, result.ids);
+            ctx.json(result);
+        } catch (IndexNotFoundException e) {
+            logger.atWarn().log("index not found.");
+            cache.put(result.uuid, new ArrayList<>());
+        } catch (Exception e) {
+            ctx.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e);
+        }
+    }
+
+    private static void documents(Context ctx) {
+        try {
+            UUID uuid = UUID.fromString(ctx.queryParam("uuid"));
+            Integer first = Integer.valueOf(ctx.queryParam("first"));
+            Integer last = Integer.valueOf(ctx.queryParam("last"));
+            List<Integer> ids = cache.get(uuid).subList(
+                first,
+                last > cache.get(uuid).size()
+                    ? cache.get(uuid).size()
+                    : last
+            );
+            List<Map.Entry<Integer,Document>> docs = lucene.documents(ids);
+            List<Map<String, Object>> result = docs.stream().map(doc -> {
+                Map<String, Object> dict = new HashMap<>() {{
+                    this.put("id", doc.getKey());
+                    for (IndexableField field: doc.getValue().getFields()) {
+                        this.put(field.name(), doc.getValue().get(field.name()));
+                    }
+                }};
+                return dict;
+            }).collect(Collectors.toList());
+            ctx.json(result);
+        } catch (IndexNotFoundException e) {
+            logger.atWarn().log("index not found.");
+        } catch (Exception e) {
+            ctx.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e);
+        }
+    }
+
+    private static void download(Context ctx) throws IOException {
+        UUID uuid = UUID.fromString(ctx.queryParam("uuid"));
+        List<Map.Entry<Integer,Document>> docs = lucene.documents(cache.get(uuid));
+
+        try (Workbook book = new XSSFWorkbook();) {
+            Sheet sheet = book.createSheet();
+            int x = 0;
+            int y = 0;
+            Row header = sheet.createRow(y++);
+            header.createCell(x++).setCellValue("id");
+            for (LuceneFieldKeys field: LuceneFieldKeys.values()) {
+                header.createCell(x++).setCellValue(field.name());
+            }
+            for (Map.Entry<Integer,Document> doc: docs) {
+                x = 0;
+                Row row = sheet.createRow(y++);
+                row.createCell(x++).setCellValue(doc.getKey());
+                for (LuceneFieldKeys field: LuceneFieldKeys.values()) {
+                    row.createCell(x++).setCellValue(doc.getValue().get(field.name()));
+                }
+            }
+            ByteArrayOutputStream buf = new ByteArrayOutputStream();
+            book.write(buf);
+            ctx.contentType(
+                "application/octet-stream"
+            ).header(
+                "Content-Disposition", "attachment; filename=\"syslog.xlsx\""
+            ).result(
+                buf.toByteArray()
+            );
+        }
     }
 }
